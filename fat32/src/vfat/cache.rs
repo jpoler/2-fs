938b9292ce9c53e64cb03e98be4938a78274c485
Jon Poler <jonathan.poler@gmail.com>
2018-09-17 22:43:30 -0700

    implement CachedDevice

--- fat32/src/vfat/cache.rs
+++ fat32/src/vfat/cache.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::hash_map::{Entry, HashMap};
 use std::{fmt, io};
 
 use traits::BlockDevice;
@@ -82,7 +82,20 @@ impl CachedDevice {
     ///
     /// Returns an error if there is an error reading the sector from the disk.
     pub fn get_mut(&mut self, sector: u64) -> io::Result<&mut [u8]> {
-        unimplemented!("CachedDevice::get_mut()")
+        let sector_size = self.device.sector_size() as usize;
+        match self.cache.entry(sector) {
+            Entry::Occupied(occupied) => {
+                let cache_entry = occupied.into_mut();
+                cache_entry.dirty = true;
+                Ok(&mut cache_entry.data[..sector_size])
+            }
+            Entry::Vacant(vacant) => {
+                let mut data = Vec::with_capacity(sector_size);
+                self.device.read_sector(sector, &mut data[..sector_size])?;
+                let cache_entry = vacant.insert(CacheEntry { data, dirty: true });
+                Ok(&mut cache_entry.data[..sector_size])
+            }
+        }
     }
 
     /// Returns a reference to the cached sector `sector`. If the sector is not
@@ -92,12 +105,66 @@ impl CachedDevice {
     ///
     /// Returns an error if there is an error reading the sector from the disk.
     pub fn get(&mut self, sector: u64) -> io::Result<&[u8]> {
-        unimplemented!("CachedDevice::get()")
+        let sector_size = self.device.sector_size() as usize;
+        match self.cache.entry(sector) {
+            Entry::Occupied(occupied) => {
+                let cache_entry = occupied.into_mut();
+                Ok(&cache_entry.data[..sector_size])
+            }
+            Entry::Vacant(vacant) => {
+                let mut data = Vec::with_capacity(sector_size);
+                self.device.read_sector(sector, &mut data[..sector_size])?;
+                let cache_entry = vacant.insert(CacheEntry { data, dirty: false });
+                Ok(&cache_entry.data[..sector_size])
+            }
+        }
     }
 }
 
-// FIXME: Implement `BlockDevice` for `CacheDevice`. The `read_sector` and
-// `write_sector` methods should only read/write from/to cached sectors.
+impl BlockDevice for CachedDevice {
+    fn read_sector(&mut self, n: u64, buf: &mut [u8]) -> io::Result<usize> {
+        let (n, factor) = self.virtual_to_physical(n);
+        let factor = factor as usize;
+        let sector_size = self.device.sector_size() as usize;
+        if buf.len() < sector_size * factor {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "insufficient buffer capacity",
+            ));
+        }
+        for (i, chunk) in buf[..(sector_size * factor)]
+            .chunks_mut(sector_size)
+            .enumerate()
+        {
+            let sector = self.get(n + i as u64)?;
+            chunk.copy_from_slice(&sector);
+        }
+
+        Ok(sector_size * factor)
+    }
+
+    fn write_sector(&mut self, n: u64, buf: &[u8]) -> io::Result<usize> {
+        let (n, factor) = self.virtual_to_physical(n);
+        let factor = factor as usize;
+        let sector_size = self.device.sector_size() as usize;
+        if buf.len() < sector_size * factor {
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidInput,
+                "insufficient buffer capacity",
+            ));
+        }
+
+        for (i, chunk) in buf[..(sector_size * factor)]
+            .chunks(sector_size)
+            .enumerate()
+        {
+            let sector = self.get_mut(n + i as u64)?;
+            sector.copy_from_slice(&chunk);
+        }
+
+        Ok(sector_size * factor)
+    }
+}
 
 impl fmt::Debug for CachedDevice {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
