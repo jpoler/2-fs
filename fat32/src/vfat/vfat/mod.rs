9fd1d1202c130f54ce2da07727a77ca63fd345da
Jon Poler <jonathan.poler@gmail.com>
2018-09-18 20:15:31 -0700

    implement VFAT helpers

--- fat32/src/vfat/vfat/mod.rs
+++ fat32/src/vfat/vfat/mod.rs
@@ -15,15 +15,15 @@ use vfat::{Cluster, Dir, Entry, Error, FatEntry, File, Shared, Status};
 #[derive(Debug)]
 pub struct VFat {
     device: CachedDevice,
-    bytes_per_sector: u16,
-    sectors_per_cluster: u8,
-    sectors_per_fat: u32,
+    bytes_per_sector: u64,
+    sectors_per_cluster: u64,
+    sectors_per_fat: u64,
     fat_start_sector: u64,
     data_start_sector: u64,
     root_dir_cluster: Cluster,
 }
 
-impl VFat {
+impl<'a> VFat {
     pub fn from<T>(mut device: T) -> Result<Shared<VFat>, Error>
     where
         T: BlockDevice + 'static,
@@ -53,41 +53,146 @@ impl VFat {
             start: partition.relative_sector as u64,
             sector_size: ebpb.bytes_per_sector as u64,
         };
-        VFat {
+        let vfat = VFat {
             device: CachedDevice::new(device, cache_partition),
-            bytes_per_sector: ebpb.bytes_per_sector,
-            sectors_per_cluster: ebpb.sectors_per_cluster,
-            sectors_per_fat: ebpb.sectors_per_fat,
-            fat_start_sector: partition.relative_sector as u64,
-            data_start_sector: partition.relative_sector as u64 + ebpb.relative_data_start(),
+            bytes_per_sector: ebpb.bytes_per_sector as u64,
+            sectors_per_cluster: ebpb.sectors_per_cluster as u64,
+            sectors_per_fat: ebpb.sectors_per_fat as u64,
+            fat_start_sector: partition.relative_sector as u64 + ebpb.relative_fat_start_sector(),
+            data_start_sector: partition.relative_sector as u64 + ebpb.relative_data_start_sector(),
             root_dir_cluster: Cluster::from(ebpb.root_cluster),
-        }
+        };
+
+        assert!(vfat.bytes_per_sector % (size_of::<FatEntry>() as u64) == 0);
+
+        vfat
     }
 
-    // TODO: The following methods may be useful here:
-    //
     //  * A method to read from an offset of a cluster into a buffer.
     //
-    //    fn read_cluster(
-    //        &mut self,
-    //        cluster: Cluster,
-    //        offset: usize,
-    //        buf: &mut [u8]
-    //    ) -> io::Result<usize>;
-    //
+    fn read_cluster(
+        &mut self,
+        cluster: Cluster,
+        offset: usize,
+        buf: &mut [u8],
+    ) -> io::Result<usize> {
+        let cluster_size_bytes = (self.bytes_per_sector * self.sectors_per_cluster) as usize;
+        assert!(buf.len() >= cluster_size_bytes);
+        let cluster_sector = self.cluster_sector(&cluster);
+
+        let mut n = 0;
+        for (i, mut chunk) in buf[..cluster_size_bytes]
+            .chunks_mut(self.bytes_per_sector as usize)
+            .enumerate()
+        {
+            n += self
+                .device
+                .read_sector(cluster_sector + i as u64, &mut chunk)?;
+        }
+
+        Ok(n)
+    }
+
     //  * A method to read all of the clusters chained from a starting cluster
     //    into a vector.
     //
-    //    fn read_chain(
-    //        &mut self,
-    //        start: Cluster,
-    //        buf: &mut Vec<u8>
-    //    ) -> io::Result<usize>;
-    //
+    fn read_chain(&mut self, start: Cluster, mut buf: &mut Vec<u8>) -> io::Result<usize> {
+        let sectors_per_cluster = self.sectors_per_cluster;
+
+        let entries =
+            FatIter::new(self, start).collect::<io::Result<Vec<(Cluster, FatEntry)>>>()?;
+
+        let mut n = 0;
+        for (cluster, entry) in entries {
+            match entry.status() {
+                Status::Data(_) => {
+                    let cluster_sector = self.cluster_sector(&cluster);
+                    for i in 0..sectors_per_cluster {
+                        n += self
+                            .device
+                            .read_all_sector(cluster_sector + i as u64, &mut buf)?;
+                    }
+                }
+                Status::Eoc(_) => break,
+                _ => {
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidData,
+                        "invalid cluster chain",
+                    ))
+                }
+            }
+        }
+
+        Ok(n)
+    }
+
     //  * A method to return a reference to a `FatEntry` for a cluster where the
     //    reference points directly into a cached sector.
     //
-    //    fn fat_entry(&mut self, cluster: Cluster) -> io::Result<&FatEntry>;
+    fn fat_entry(&mut self, cluster: Cluster) -> io::Result<FatEntry> {
+        let n = cluster.get();
+        let sector = self.fat_entry_sector(n);
+        let offset = self.fat_sector_offset(n);
+        let buf = self.device.get(sector)?;
+        let fat_entries = unsafe { buf.cast::<FatEntry>() };
+        Ok(fat_entries[offset])
+    }
+
+    fn cluster_sector(&self, cluster: &Cluster) -> u64 {
+        self.data_start_sector + self.sectors_per_cluster * (cluster.get() as u64 - 2)
+    }
+
+    fn fat_entry_sector(&self, n: u32) -> u64 {
+        self.fat_start_sector + (n as u64 / self.fats_per_sector())
+    }
+
+    fn fat_sector_offset(&self, n: u32) -> usize {
+        (n as usize % self.fats_per_sector() as usize)
+    }
+
+    fn fats_per_sector(&self) -> u64 {
+        self.bytes_per_sector / size_of::<FatEntry>() as u64
+    }
+
+    fn fat_size_bytes(&self) -> u64 {
+        self.bytes_per_sector * self.sectors_per_fat
+    }
+}
+
+struct FatIter<'a> {
+    vfat: &'a mut VFat,
+    current: Option<Cluster>,
+}
+
+impl<'a> FatIter<'a> {
+    fn new(vfat: &'a mut VFat, cluster: Cluster) -> FatIter {
+        FatIter {
+            vfat,
+            current: Some(cluster),
+        }
+    }
+}
+
+impl<'a> Iterator for FatIter<'a> {
+    type Item = io::Result<(Cluster, FatEntry)>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(cluster) = self.current {
+            match self.vfat.fat_entry(cluster) {
+                Ok(entry) => {
+                    match entry.status() {
+                        Status::Data(next_cluster) => self.current = Some(next_cluster),
+                        _ => self.current = None,
+                    }
+                    Some(Ok((cluster, entry)))
+                }
+
+                Err(err) => Some(Err(err)),
+            }
+        } else {
+            None
+        }
+    }
 }
 
 impl<'a> FileSystem for &'a Shared<VFat> {
